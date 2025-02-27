3907ce251a3979d42ef4d9247562561dc4a99b95
ScienceMind11 <sciencemind@icloud.com>
2025-02-27 11:29:44 -0500

    Sidebar animation, codex texture fix

--- src/main/java/io/teking/eternitek/core/client/screen/codex/screens/CodexScreen.java
+++ src/main/java/io/teking/eternitek/core/client/screen/codex/screens/CodexScreen.java
@@ -14,15 +14,20 @@ import java.util.List;
 
 @Environment(EnvType.CLIENT)
 public class CodexScreen extends Screen {
+
     private static final Identifier MAIN_PAGE_TEXTURE = EternitekCore.id("textures/gui/codex.png");
     private static final Identifier SIDEBAR_TEXTURE = EternitekCore.id("textures/gui/codex_sidebar.png");
     private static final Identifier BOOK_ICON = EternitekCore.id("textures/item/codex.png");
+
     private static final int SIDEBAR_COLLAPSED_WIDTH = 26;
     private static final int SIDEBAR_EXPANDED_WIDTH = 100;
+    private int currentSidebarWidth = SIDEBAR_COLLAPSED_WIDTH;
+
     private boolean isSidebarExpanded = false;
-    private float currentSidebarWidth = SIDEBAR_COLLAPSED_WIDTH;
-    private final int mainPageWidth = 384;
+
+    private final int mainPageWidth = 385;
     private final int bookHeight = 256;
+
     private int bookRenderX;
     private int bookRenderY;
 
@@ -56,59 +61,31 @@ public class CodexScreen extends Screen {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+
         super.render(context, mouseX, mouseY, delta);
 
-        if (isSidebarExpanded) {
-            currentSidebarWidth += (float) ((SIDEBAR_EXPANDED_WIDTH - currentSidebarWidth) * 0.1);
-            if (currentSidebarWidth > SIDEBAR_EXPANDED_WIDTH - 1) {
-                currentSidebarWidth = SIDEBAR_EXPANDED_WIDTH;
-            }
+        if(isSidebarExpanded) {
+            if(currentSidebarWidth < SIDEBAR_EXPANDED_WIDTH) currentSidebarWidth++;
         } else {
-            currentSidebarWidth += (float) ((SIDEBAR_COLLAPSED_WIDTH - currentSidebarWidth) * 0.1);
-            if (currentSidebarWidth < SIDEBAR_COLLAPSED_WIDTH + 1) {
-                currentSidebarWidth = SIDEBAR_COLLAPSED_WIDTH;
-            }
+            if(currentSidebarWidth > SIDEBAR_COLLAPSED_WIDTH) currentSidebarWidth--;
         }
 
-        context.drawTexture(MAIN_PAGE_TEXTURE, bookRenderX, bookRenderY, 0, 0, mainPageWidth, bookHeight, mainPageWidth, bookHeight);
-        context.drawTexture(SIDEBAR_TEXTURE, bookRenderX + mainPageWidth - (int) currentSidebarWidth, bookRenderY, 0, 0, (int) currentSidebarWidth, bookHeight, (int) currentSidebarWidth, bookHeight);
+        context.drawTexture(
+                MAIN_PAGE_TEXTURE,
+                bookRenderX, bookRenderY,
+                0, 0,
+                mainPageWidth, bookHeight,
+                512, 256
+        );
 
-        int bookIconX = bookRenderX + mainPageWidth - SIDEBAR_COLLAPSED_WIDTH + 3;
-        if (isSidebarExpanded) bookIconX -= 5;
-        int bookIconY = bookRenderY + bookHeight - 30;
-        context.drawTexture(BOOK_ICON, bookIconX, bookIconY, 0, 0, 20, 20, 20, 20);
+        context.drawTexture(
+                MAIN_PAGE_TEXTURE,
+                (bookRenderX + 370) - currentSidebarWidth, bookRenderY + 5,
+                385, 0,
+                8, 246,
+                512, 256
+        );
 
-        if (currentSidebarWidth > SIDEBAR_COLLAPSED_WIDTH) {
-            int buttonY = bookRenderY + 15;
-            int padding = 2;
-            int iconSize = 16;
-            int buttonHeight = iconSize + padding * 2;
-            int sidebarWidth = (int) currentSidebarWidth;
-
-            for (int i = 0; i < tiers.size(); i++) {
-                Tier tier = tiers.get(i);
-                int buttonX = bookRenderX + mainPageWidth - sidebarWidth;
-
-                int iconX = buttonX + (SIDEBAR_COLLAPSED_WIDTH - iconSize) / 2;
-
-                if (i == selectedTier) {
-                    context.fill(buttonX + 6, buttonY, buttonX + sidebarWidth - 6, buttonY + buttonHeight, 0xFF8B0000);
-                }
-
-                context.drawTexture(tier.icon, iconX + 4, buttonY + padding, 0, 0, iconSize, iconSize, iconSize, iconSize);
-
-                if (sidebarWidth > SIDEBAR_COLLAPSED_WIDTH + 10) {
-                    int textX = buttonX + SIDEBAR_COLLAPSED_WIDTH + 2;
-                    context.drawText(textRenderer, tier.name, textX, buttonY + (buttonHeight - textRenderer.fontHeight) / 3, 0xFFFFFF, true);
-                }
-
-                buttonY += buttonHeight;
-
-                if (buttonY + buttonHeight > bookRenderY + bookHeight - 40) {
-                    break;
-                }
-            }
-        }
     }
 
     @Override
