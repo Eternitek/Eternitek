b0dcf6437ec18b386543edf029d69f28f501df44
IDoTheHax <hax@idothehax.com>
2025-02-27 15:25:39 +0100

    tried to fix the multisided furnace textures

--- src/main/java/io/teking/eternitek/core/data/EternitekModelProvider.java
+++ src/main/java/io/teking/eternitek/core/data/EternitekModelProvider.java
@@ -1,10 +1,15 @@
 package io.teking.eternitek.core.data;
 
+import io.teking.eternitek.core.registry.EternitekBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
-import net.minecraft.data.client.BlockStateModelGenerator;
-import net.minecraft.data.client.ItemModelGenerator;
-import net.minecraft.data.client.Models;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.data.client.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
 
 import static io.teking.eternitek.core.registry.EternitekItems.*;
 import static io.teking.eternitek.core.registry.EternitekBlocks.*;
@@ -18,7 +23,7 @@ public class EternitekModelProvider extends FabricModelProvider {
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
         blockStateModelGenerator.registerSimpleCubeAll(BLAST_BRICKS);
-        blockStateModelGenerator.registerSimpleCubeAll(PRIMITIVE_FURNACE);
+        blockStateModelGenerator.registerSingleton(PRIMITIVE_FURNACE, TexturedModel.ORIENTABLE_WITH_BOTTOM);
     }
 
     @Override
