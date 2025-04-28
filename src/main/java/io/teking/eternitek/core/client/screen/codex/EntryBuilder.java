184067dcc27253a47af0a0ca19dfd5ef78d1bf3c
ScienceMind11 <sciencemind@icloud.com>
2025-04-27 22:58:25 -0400

    Rip out tooltip shenanigans (will be replaced later)
    Took 20 minutes

--- src/main/java/io/teking/eternitek/core/client/screen/codex/EntryBuilder.java
+++
@@ -1,7 +0,0 @@
-package io.teking.eternitek.core.client.screen.codex;
-
-public class EntryBuilder {
-
-
-
-}
--- src/main/java/io/teking/eternitek/core/client/screen/codex/pages/CategoryPage.java
+++
@@ -1,4 +0,0 @@
-package io.teking.eternitek.core.client.screen.codex.pages;
-
-public class CategoryPage {
-}
