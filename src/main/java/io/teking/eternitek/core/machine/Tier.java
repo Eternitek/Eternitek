33da91a66141bc5e8b61b9781503faf587e76ff9
ScienceMind11 <sciencemind@icloud.com>
2025-05-02 11:26:23 -0400

    add list to store all tiers

--- src/main/java/io/teking/eternitek/core/machine/Tier.java
+++ src/main/java/io/teking/eternitek/core/machine/Tier.java
@@ -2,17 +2,23 @@ package io.teking.eternitek.core.machine;
 
 import net.minecraft.util.Identifier;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Tier {
 
-    public static final Tier T0 = new Tier("Crude",      "Heat Resistant Brick",   Identifier.of("textures/block/oak_planks.png"),      0);
-    public static final Tier T1 = new Tier("Makeshift",  "Brass",                  Identifier.of("textures/block/stone.png"),           1);
-    public static final Tier T2 = new Tier("Industrial", "Steel",                  Identifier.of("textures/block/iron_block.png"),      2);
-    public static final Tier T3 = new Tier("Advanced",   "Aluminum",               Identifier.of("textures/block/gold_block.png"),      3);
-    public static final Tier T4 = new Tier("Refined",    "Stainless Steel",        Identifier.of("textures/block/diamond_block.png"),   4);
-    public static final Tier T5 = new Tier("Reclaimed",  "Sci-fi BS",              Identifier.of("textures/block/emerald_block.png"),   5);
-    public static final Tier T6 = new Tier("Reawakened", "Recharged Power Source", Identifier.of("textures/block/netherite_block.png"), 6);
-    public static final Tier T7 = new Tier("Resonant",   "Sci-fi BS",              Identifier.of("textures/block/obsidian.png"),        7);
-    public static final Tier T8 = new Tier("Evolved",    "Unknown",                Identifier.of("textures/block/bedrock.png"),         8);
+    public static final List<Tier> TIERS = new ArrayList<>();
+
+    public static final Tier T0 = addTier(new Tier("Crude",      "Heat Resistant Brick",   Identifier.of("textures/block/oak_planks.png"),      0));
+    public static final Tier T1 = addTier(new Tier("Makeshift",  "Brass",                  Identifier.of("textures/block/stone.png"),           1));
+    public static final Tier T2 = addTier(new Tier("Industrial", "Steel",                  Identifier.of("textures/block/iron_block.png"),      2));
+    public static final Tier T3 = addTier(new Tier("Advanced",   "Aluminum",               Identifier.of("textures/block/gold_block.png"),      3));
+    public static final Tier T4 = addTier(new Tier("Refined",    "Stainless Steel",        Identifier.of("textures/block/diamond_block.png"),   4));
+    public static final Tier T5 = addTier(new Tier("Reclaimed",  "Sci-fi BS",              Identifier.of("textures/block/emerald_block.png"),   5));
+    public static final Tier T6 = addTier(new Tier("Reawakened", "Recharged Power Source", Identifier.of("textures/block/netherite_block.png"), 6));
+    public static final Tier T7 = addTier(new Tier("Resonant",   "Sci-fi BS",              Identifier.of("textures/block/obsidian.png"),        7));
+    public static final Tier T8 = addTier(new Tier("Evolved",    "Unknown",                Identifier.of("textures/block/bedrock.png"),         8));
 
     public String name;
     public String mainResource;
@@ -24,6 +30,11 @@ public class Tier {
         this.mainResource = mainResource;
         this.icon = icon;
         this.power = power;
+    }
+
+    public static Tier addTier(Tier tier) {
+        TIERS.add(tier);
+        return tier;
     }
 
 }
