2f474cd3cb5cdbc4df37e0590bf0537a24c2672b
ScienceMind11 <sciencemind@icloud.com>
2025-10-14 14:34:55 -0400

    update & optimize pipe transfer logic

--- src/main/java/io/teking/eternitek/core/util/pipes/ItemTransferHelper.java
+++ src/main/java/io/teking/eternitek/core/util/pipes/ItemTransferHelper.java
@@ -1,14 +1,21 @@
 package io.teking.eternitek.core.util.pipes;
 
+import com.google.common.base.Predicates;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.fabricmc.fabric.api.transfer.v1.storage.StorageUtil;
+import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
 import net.minecraft.block.InventoryProvider;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.inventory.SidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 
 public class ItemTransferHelper {
+
+    public static final long TRANSFER_RATE = 8;
 
     public static boolean transferItem(Inventory sourceInventory, Inventory targetInventory, int sourceSlot, int maxAmount) {
         ItemStack sourceStack = sourceInventory.getStack(sourceSlot);
@@ -50,6 +57,26 @@ public class ItemTransferHelper {
 
         return null;
     }
+
+    public static <T> boolean tryMove(Storage<T> from, Storage<T> to) {
+        if (from == null || to == null) return false;
+        try (Transaction transaction = Transaction.openOuter()) {
+            long moved = StorageUtil.move(
+                    from,
+                    to,
+                    Predicates.alwaysTrue(),
+                    TRANSFER_RATE,
+                    transaction
+            );
+            if (moved > 0) {
+                transaction.commit();
+                return true;
+            }
+            transaction.abort();
+        }
+        return false;
+    }
+
 
     public static boolean canCombine(ItemStack stack1, ItemStack stack2) {
         return ItemStack.areItemsAndComponentsEqual(stack1, stack2);
