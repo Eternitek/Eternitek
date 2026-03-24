d62b2b441989fbc0cc94fc1f83b5d4b7e33ea7b7
ScienceMind11 <sciencemind@icloud.com>
2026-03-24 19:35:52 -0400

    refactor style of method chaining in research provider
    Took 15 seconds

--- src/main/java/net/mercury/eternitek/core/data/EternitekResearchProvider.java
+++ src/main/java/net/mercury/eternitek/core/data/EternitekResearchProvider.java
@@ -17,25 +17,35 @@ public class EternitekResearchProvider extends ResearchProvider {
     @Override
     protected void generateResearch(TreeBuilder builder) {
 
-        builder
-                .name("Keycards")
-                .id(EternitekCore.id("keycards"))
-                .node()
-                        .id("eternitek:simple")
-                        .tooltip(Component.literal("Simple").withColor(0xFFA5AFB1))
-                        .tooltip(Component.literal("The name says it all.").withColor(0xFFA5AFB1))
-                        .icon("eternitek:textures/item/simple_keycard.png")
-                        .position(0, 0)
-                        .connection("eternitek:gilded")
-                        .build()
-                .node()
-                        .id("eternitek:gilded")
-                        .tooltip(Component.literal("Gilded").withColor(0xFFE9BE6A))
-                        .tooltip(Component.literal("For executives only.").withColor(0xFFA5AFB1))
-                        .icon("eternitek:textures/item/gilded_keycard.png")
-                        .position(60, 0)
-                        .build()
+        builder.name("Keycards").id(EternitekCore.id("keycards"));
+
+        builder.node()
+                .id("eternitek:simple")
+                .tooltip(Component.literal("Simple").withColor(0xFFA5AFB1))
+                .tooltip(Component.literal("The name says it all.").withColor(0xFFA5AFB1))
+                .icon("eternitek:textures/item/simple_keycard.png")
+                .position(0, 0)
+                .connection("eternitek:reverious")
+                .connection("eternitek:gilded")
                 .build();
+
+        builder.node()
+                .id("eternitek:reverious")
+                .tooltip(Component.literal("Reverious").withColor(0xFFCC4F82))
+                .tooltip(Component.literal("Is it... breathing?").withColor(0xFFA5AFB1))
+                .icon("eternitek:textures/item/reverious_keycard.png")
+                .position(-60, 60)
+                .build();
+
+        builder.node()
+                .id("eternitek:gilded")
+                .tooltip(Component.literal("Gilded").withColor(0xFFE9BE6A))
+                .tooltip(Component.literal("For executives only.").withColor(0xFFA5AFB1))
+                .icon("eternitek:textures/item/gilded_keycard.png")
+                .position(60, -60)
+                .build();
+
+        builder.build();
 
     }
 
